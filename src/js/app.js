22b4255b35c3db30c42b46341845670432dc6eb9
J Han <69409369+ilsahaann@users.noreply.github.com>
2021-06-29 21:26:12 +0900

    attemping to draw

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,46 @@ import '../icon-192.png'
 import '../icon-512.png'
 import '../css/style.scss'
 
+//comment
 // This is an important comment
+
+const canvas= docuument.quertSelector('canvas')
+const context= canvas.getContext('2d')
+
+X=math.random()*(canvas.width-2*r)
+Y=math.random()*(canvas.height-2*r)
+vx=Math.random()*10*cos(Math.random()*Math.PI*2)
+vy=Math.random()*10*sin(Math.random()*Math.PI*2)
+
+class Ball{
+    constructor(){
+        this.r=Math.random()*40+40
+        this.x=Math.random()*(canvas.width-2*r)
+        this.y=Math.random()*(canvas.height-2*r)+this.r 
+        this.speed=Math.random()*10+10
+        const rotation=Math.random()*Math.PI*2
+        this.vx=this.speed*Math.cos(rotation)
+        this.vy=this.speed*Math.sin(rotation)
+        
+    move() {
+        this.x+=this.vx
+        this.y+=this.vy
+        if(this.x<this.r||this.x>canvas.width-r) this.vx*=-1
+        if(this.y<this.r||this.y>canvas.height-r) this.vy*=-1
+        }
+        
+        this.color=Color.black
+    collide(){
+        for(const ball of balls){
+            const d=Math.hypot(this.x-ball.x,this.y-ball,y)
+            if (this!== ball && d<this.r+ball.r){
+                this.color='red'
+                break
+                }
+            }
+        }
+    }
+    
+}
+
+let 
