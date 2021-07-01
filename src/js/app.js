a1aec4cf82118b16f7a22d775f530e6f3d3d3f26
stvenlee <86606857+stvenlee@users.noreply.github.com>
2021-07-01 19:19:47 +0900

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,28 @@ import '../icon-192.png'
 import '../icon-512.png'
 import '../css/style.scss'
 
-// This is an important comment
+const canvas =  document.querySelector('canvas')
+const context = canvas.getContext('2d')
+canvas.width = 1600
+canvas.height = 900
+
+class Ball {
+    constructor() {
+        
+        const r = Math.random() * 20 + 20
+        const x = Math.random() * (canvas.width - 2 * r) + r
+        const y = Math.random() * (canvas.height - 2 * r) + r
+        
+        context.fillStyle = 'blue'
+        
+        function drawCircle(x, y) {
+            context.beginPath()
+            context.arc(x, y, r, 0, Math.PI * 2)
+            context.fill()
+        }
+        
+        drawCircle(x, y)
+    }
+}
+
+const balls = new Ball()
