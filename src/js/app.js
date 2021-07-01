cff3686ad60f16f51417792d28e638ccd51c810a
jungyoonlee212 <86659203+jungyoonlee212@users.noreply.github.com>
2021-07-01 06:50:20 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -28,7 +28,7 @@ class Ball {
           this.vy *= -1
       }
     draw() {
-        context.fillStyle = this.color
+        context.fillStyle = 'pink'
         context.beginPath()
         context.arc(this.x, this.y, this.r, 0, Math.PI * 2)
         context.fill() 
