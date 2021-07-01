059078898382083ed42dce326c85ef0ab4a70c79
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-07-01 21:51:49 +0900

    Squashed commit of the following:
    commit 2e1de5e4911955be734e60992d0ff35a6d1b2fa4
    Author: J Han <69409369+ilsahaann@users.noreply.github.com>
    Date:   Thu Jul 1 21:35:07 2021 +0900
    
        Update app.js
    
    commit 2aeb374a56e2c69d0c5bc67a12512607f247af04
    Author: J Han <69409369+ilsahaann@users.noreply.github.com>
    Date:   Thu Jul 1 21:24:05 2021 +0900
    
        Update app.js
    
    commit a4e11082b5d8fac180690e72d8e281277aefa71d
    Author: J Han <69409369+ilsahaann@users.noreply.github.com>
    Date:   Thu Jul 1 20:59:03 2021 +0900
    
        Update app.js
    
    commit b07958a6c058cf144b11f0c5c7feac6fdcfe8c1b
    Author: J Han <69409369+ilsahaann@users.noreply.github.com>
    Date:   Thu Jul 1 20:56:36 2021 +0900
    
        Update app.js
    
    commit 4f211238a1a44dacb2b3edd43be174f579deb908
    Author: J Han <69409369+ilsahaann@users.noreply.github.com>
    Date:   Thu Jul 1 20:27:04 2021 +0900
    
        moving
    
    commit 7f62b783e0014dc9689dd0be4428054e853f4f60
    Author: J Han <69409369+ilsahaann@users.noreply.github.com>
    Date:   Thu Jul 1 19:59:47 2021 +0900
    
        changes
    
    commit 376372d31adbfad34fc4b1fbbeb9c3bca5455680
    Author: J Han <69409369+ilsahaann@users.noreply.github.com>
    Date:   Thu Jul 1 19:51:18 2021 +0900
    
        changes
    
    commit e1e3ab727e49b05ab2de0b71678eb81e4b6c1cba
    Author: J Han <69409369+ilsahaann@users.noreply.github.com>
    Date:   Thu Jul 1 19:10:32 2021 +0900
    
        class ball
    
    commit 0d882d23e7626508da59a2c80f2c5e885eafe4d7
    Author: J Han <69409369+ilsahaann@users.noreply.github.com>
    Date:   Tue Jun 29 21:44:13 2021 +0900
    
        attempt 2
    
    commit 22b4255b35c3db30c42b46341845670432dc6eb9
    Author: J Han <69409369+ilsahaann@users.noreply.github.com>
    Date:   Tue Jun 29 21:26:12 2021 +0900
    
        attemping to draw
    
    commit 517fe9def3368bb9edf614b7dd1e82ccf434cd50
    Merge: 42a646b 774046e
    Author: J Han <69409369+ilsahaann@users.noreply.github.com>
    Date:   Tue Jun 29 20:38:54 2021 +0900
    
        Merge branch 'main' into jeewon
    
    commit 774046e646fa5a78514b9e8b6af115565599ff46
    Author: Jiseung Jung <roadrunner23@yonsei.ac.kr>
    Date:   Tue Jun 29 20:33:01 2021 +0900
    
        Add a comment
    
    commit 42a646bf2d3507d6dcc45781e7aaa3071013b97b
    Author: J Han <69409369+ilsahaann@users.noreply.github.com>
    Date:   Tue Jun 29 20:26:12 2021 +0900
    
        comment

--- src/js/app.js
+++ src/js/app.js
@@ -6,28 +6,77 @@ import '../css/style.scss'
 
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
-
 canvas.width = 1600
 canvas.height = 900
 
-class Ball {
-  constructor() {
-    this.r = Math.random() * 20 + 20
-    this.x = Math.random() * (canvas.width - 2 * this.r) + this.r
-    this.y = Math.random() * (canvas.height - 2 * this.r) + this.r
-    this.originalColor = `rgb(0, 
-      ${Math.random() * 255},
-      ${Math.random() * 255})`
-    this.color = this.originalColor
-  } 
-  
-  draw() {
-    context.fillStyle = this.color
-    context.beginPath()
-    context.arc(this.x, this.y, this.r, 0, Math.PI * 2)
-    context.fill()
-  }
+class Ball{
+    constructor() {
+        this.r = Math.random()*20+20
+        this.x = Math.random()*(canvas.width-2*this.r)
+        this.y = Math.random()*(canvas.height-2*this.r)+this.r
+        this.speed = Math.random()*5+5
+        const rotation=Math.random()*Math.PI*2
+        this.vx = this.speed*Math.cos(rotation)
+        this.vy = this.speed*Math.sin(rotation)
+        this.originalColor = `rgb(244,194,194)`
+        this.color = this.originalColor
+    }
+
+    move() {
+        this.x += this.vx
+        this.y += this.vy
+        if (this.x < this.r || this.x > canvas.width - this.r)
+            this.vx *= -1
+        if (this.y < this.r || this.y > canvas.height - this.r)
+            this.vy *= -1
+    }
+
+    draw() {
+        context.fillStyle = this.color
+        context.beginPath()
+        context.arc(this.x, this.y, this.r, 0, Math.PI * 2)
+        context.fill()
+    } 
+    
+    collide() {
+        this.color = this.originalColor
+        for (const ball of ballsArray) {
+            const d = Math.hypot (this.x-ball.x,this.y-ball.y)
+            if (this !== ball && d<this.r+ball.r) {
+                this.color = 'purple'
+                break
+            }
+        }
+    }
 }
 
 const ball = new Ball()
 ball.draw()
+
+let numBalls = 20
+const ballsArray = []
+
+for (let i=0; i < numBalls; i++) {
+    ballsArray.push(new Ball())
+}
+
+function update() {
+    for (const ball of ballsArray){
+        ball.move()
+        ball.collide()
+    }
+}
+
+function render() {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    for(const ball of ballsArray) {
+        ball.draw()
+    }
+}
+
+function loop(){
+    update()
+    render()
+    requestAnimationFrame(loop)
+}
+loop()
