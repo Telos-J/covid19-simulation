294d494d18645731d69728d83f053bb80d0f64bb
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-07-03 15:44:56 +0900

    format code

--- src/js/app.js
+++ src/js/app.js
@@ -9,15 +9,15 @@ const context = canvas.getContext('2d')
 canvas.width = 1600
 canvas.height = 900
 
-class Ball{
+class Ball {
     constructor() {
-        this.r = Math.random()*20+20
-        this.x = Math.random()*(canvas.width-2*this.r)
-        this.y = Math.random()*(canvas.height-2*this.r)+this.r
-        this.speed = Math.random()*5+5
-        const rotation=Math.random()*Math.PI*2
-        this.vx = this.speed*Math.cos(rotation)
-        this.vy = this.speed*Math.sin(rotation)
+        this.r = Math.random() * 20 + 20
+        this.x = Math.random() * (canvas.width - 2 * this.r)
+        this.y = Math.random() * (canvas.height - 2 * this.r) + this.r
+        this.speed = Math.random() * 5 + 5
+        const rotation = Math.random() * Math.PI * 2
+        this.vx = this.speed * Math.cos(rotation)
+        this.vy = this.speed * Math.sin(rotation)
         this.originalColor = `rgb(244,194,194)`
         this.color = this.originalColor
     }
@@ -36,13 +36,13 @@ class Ball{
         context.beginPath()
         context.arc(this.x, this.y, this.r, 0, Math.PI * 2)
         context.fill()
-    } 
-    
+    }
+
     collide() {
         this.color = this.originalColor
         for (const ball of ballsArray) {
-            const d = Math.hypot (this.x-ball.x,this.y-ball.y)
-            if (this !== ball && d<this.r+ball.r) {
+            const d = Math.hypot(this.x - ball.x, this.y - ball.y)
+            if (this !== ball && d < this.r + ball.r) {
                 this.color = 'purple'
                 break
             }
@@ -56,12 +56,12 @@ ball.draw()
 let numBalls = 20
 const ballsArray = []
 
-for (let i=0; i < numBalls; i++) {
+for (let i = 0; i < numBalls; i++) {
     ballsArray.push(new Ball())
 }
 
 function update() {
-    for (const ball of ballsArray){
+    for (const ball of ballsArray) {
         ball.move()
         ball.collide()
     }
@@ -69,12 +69,12 @@ function update() {
 
 function render() {
     context.clearRect(0, 0, canvas.width, canvas.height)
-    for(const ball of ballsArray) {
+    for (const ball of ballsArray) {
         ball.draw()
     }
 }
 
-function loop(){
+function loop() {
     update()
     render()
     requestAnimationFrame(loop)
