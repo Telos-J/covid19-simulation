fec3b17eaf11bc78bf658b9c38290ef656d60745
jungyoonlee212 <86659203+jungyoonlee212@users.noreply.github.com>
2021-07-03 08:42:30 -0400

    redoing our code with pixi js

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import '../image.png'
 import '../icon-192.png'
 import '../icon-512.png'
 import '../css/style.scss'
-import { balls } from './ball'
+import { balls, setupBalls } from './ball'
 import * as PIXI from 'pixi.js'
 
 const type = PIXI.utils.isWebGLSupported() ? 'WebGL' : 'canvas'
@@ -13,17 +13,19 @@ const canvas = document.querySelector('canvas')
 
 let app = new PIXI.Application({
     view: canvas,
-	width: innerWidth,
-    height: innerHeight,
+	width: 1600,
+    height: 900,
     backgroundAlpha: 0,
     resolution: devicePixelRatio || 1
 });
 
+PIXI.Loader.shared.load(setupBalls)
 app.ticker.add(loop)
 
 function loop(deltaTime) {
     for (const ball of balls.children) {
         ball.move()
+        ball.collide()
     }
 }
 
