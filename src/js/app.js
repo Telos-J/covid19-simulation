c383909c440e0174f0aba0b2d3631d0b8c3d1115
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-07-03 20:33:35 +0900

    working progress

--- src/js/app.js
+++ src/js/app.js
@@ -14,28 +14,26 @@ let app = new PIXI.Application({
     view: canvas,
     width: 1600,
     height: 900,
-    backgroundAlpha: 1,
+    backgroundAlpha: 0,
     resolution: devicePixelRatio || 1
 });
 
-function update() {
-    for (const ball of ballsArray) {
-        ball.move()
-        ball.collide()
-    }
-}
+let balls = new PIXI.Container();
+app.stage.addChild(balls);
 
-function render() {
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    for (const ball of ballsArray) {
-        ball.draw()
-    }
-}
+const ball = new PIXI.Sprite()
 
-function loop() {
-    update()
-    render()
-    requestAnimationFrame(loop)
-}
+const graphic = new PIXI.Graphics()
+graphic.beginFill()
+graphic.arc(0, 0, 50, 0, Math.PI * 2)
+graphic.endFill()
 
-// loop()
+ball.texture = app.renderer.generateTexture(graphic)
+balls.addChild(ball)
+
+
+app.ticker.add(gameLoop)
+
+function gameLoop(deltaTime) {
+    ball.x += 5 * deltaTime
+}
