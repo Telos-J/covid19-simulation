6718f13b5b0c893440ca6e49feafd375ef8b3f82
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-07-03 20:09:25 +0900

    uppdate

--- src/js/app.js
+++ src/js/app.js
@@ -3,62 +3,22 @@ import '../image.png'
 import '../icon-192.png'
 import '../icon-512.png'
 import '../css/style.scss'
+import * as PIXI from 'pixi.js'
+
 
 const canvas = document.querySelector('canvas')
-const context = canvas.getContext('2d')
-canvas.width = 1600
-canvas.height = 900
 
-class Ball {
-    constructor() {
-        this.r = Math.random() * 20 + 20
-        this.x = Math.random() * (canvas.width - 2 * this.r)
-        this.y = Math.random() * (canvas.height - 2 * this.r) + this.r
-        this.speed = Math.random() * 5 + 5
-        const rotation = Math.random() * Math.PI * 2
-        this.vx = this.speed * Math.cos(rotation)
-        this.vy = this.speed * Math.sin(rotation)
-        this.originalColor = `rgb(244,194,194)`
-        this.color = this.originalColor
-    }
+const type=PIXI.utils.isWebGLSupported() ? 'WebGL':'canvas'
+PIXI.utils.sayHello(type)
 
-    move() {
-        this.x += this.vx
-        this.y += this.vy
-        if (this.x < this.r || this.x > canvas.width - this.r)
-            this.vx *= -1
-        if (this.y < this.r || this.y > canvas.height - this.r)
-            this.vy *= -1
-    }
+let app = new PIXI.Application({
+    view: canvas,
+    width: 1600,
+    height: 900,
+    backgroundAlpha: 1,
+    resolution: devicePixelRatio || 1
+});
 
-    draw() {
-        context.fillStyle = this.color
-        context.beginPath()
-        context.arc(this.x, this.y, this.r, 0, Math.PI * 2)
-        context.fill()
-    }
-
-    collide() {
-        this.color = this.originalColor
-        for (const ball of ballsArray) {
-            const d = Math.hypot(this.x - ball.x, this.y - ball.y)
-            if (this !== ball && d < this.r + ball.r) {
-                this.color = 'purple'
-                break
-            }
-        }
-    }
-}
-
-const ball = new Ball()
-ball.draw()
-
-let numBalls = 20
-const ballsArray = []
-
-for (let i = 0; i < numBalls; i++) {
-    ballsArray.push(new Ball())
-}
 
 function update() {
     for (const ball of ballsArray) {
@@ -79,6 +39,5 @@ function loop() {
     render()
     requestAnimationFrame(loop)
 }
-loop()
 
-export {canvas,context}
+// loop()
