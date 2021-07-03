ad01d7f06a33b7c59799ced5f84ab3c734455ddf
J Han <69409369+ilsahaann@users.noreply.github.com>
2021-07-03 20:32:21 +0900

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ import '../icon-192.png'
 import '../icon-512.png'
 import '../css/style.scss'
 import * as PIXI from 'pixi.js'
+import { Renderer } from 'pixi.js'
 
 
 const canvas = document.querySelector('canvas')
@@ -15,10 +16,28 @@ let app = new PIXI.Application({
     view: canvas,
     width: 1600,
     height: 900,
-    backgroundAlpha: 1,
+    backgroundAlpha: 0,
     resolution: devicePixelRatio || 1
 });
 
+let balls=new PIXI.Container()
+app.stage.addChild(balls)
+
+const ball=new PIXI.Sprite()
+
+const graphic=new PIXI.Graphics()
+graphic.beginFill()
+graphic.arc(0,0,50,Math.PI)
+graphic.endFill()
+
+ball.texture=app.renderer.generateTexture(graphic)
+balls.addChild(ball)
+
+app.ticker.add(gameLoop)
+
+function gameLoop(deltaTime){
+    ball.x+=5*deltaTime
+}
 
 function update() {
     for (const ball of ballsArray) {
