c536bbc6aebed05c092822ec0da20829521f9028
stvenlee <86606857+stvenlee@users.noreply.github.com>
2021-07-06 22:08:30 +0900

    attempt

--- src/js/ball.js
+++ src/js/ball.js
@@ -1,5 +1,6 @@
 import * as PIXI from 'pixi.js'
 import { app } from './app'
+import {add, sub, dot, magnitude, scale, normalize} from './vector'
 
 class Ball extends PIXI.Graphics {
     constructor() {
@@ -7,13 +8,13 @@ class Ball extends PIXI.Graphics {
         this.r = Math.random() * 10 + 10
         this.x = Math.random() * (app.screen.width - 2 * this.r) + this.r
         this.y = Math.random() * (app.screen.height - 2 * this.r) + this.r
-        this.rotation = Math.random() * Math.PI * 2
-        this.speed = Math.random() * 2 + 2
         this.velocity = new PIXI.Point(
             this.speed * Math.cos(this.rotation),
             this.speed * Math.sin(this.rotation)
         )
-        this.originalColor = Math.random() * 0x00ffff
+        this.rotation = Math.random() * Math.PI * 2
+        this.speed = Math.random() * 2 + 2
+        this.originalColor = Math.random() < 0.01 ? 0xff0000 : Math.random() * 0x00ffff
         this.tint = this.originalColor
         this.beginFill(0xffffff)
         this.arc(0, 0, this.r, 0, Math.PI * 2)
@@ -38,12 +39,23 @@ class Ball extends PIXI.Graphics {
 
 
     move() {
-        this.x += this.vx
-        this.y += this.vy
-        if (this.x < this.r || this.x > app.screen.width - this.r)
+        this.position = add(this.position, this.velocity)
+        if (this.x < this.r) {
+            this.x = this.r
             this.vx *= -1
-        if (this.y < this.r || this.y > app.screen.height - this.r)
+        }
+        if (this.x > app.screen.width - this.r) {
+            this.x = app.screen.width - this.r
+            this.vx *= -1
+        }
+        if (this.y < this.r){
+            this.y =this.r
+            this,vy *= -1
+        }
+        if (this.y > app.screen.height - this.r) {
+            this.y = app.screen.height - this.r
             this.vy *= -1
+        }
     }
 
     collide() {
@@ -51,9 +63,30 @@ class Ball extends PIXI.Graphics {
         for (const ball of balls.children) {
             const d = Math.hypot(this.x - ball.x, this.y - ball.y)
             if (this !== ball && d < this.r + ball.r) {
-                this.tint = 0xff0000
+                this.bounce(ball)
+                this.contage(ball)
                 break
             }
+        }
+    }
+    
+    bounce(ball) {
+        const m = scale(add(this.position, ball.position), 0.5)
+        const n1 = normalize(sub(this.position, ball.position), this.r)
+        const n2 = normalize(sub(ball.position, this.position), ball.r)
+
+        this.position = add(m, n1)
+        ball.position = add(m, n2)
+
+        const velocity = this.velocity
+        this.velocity = ball.velocity
+        ball.velocity = velocity
+    }
+
+    contage(ball) {
+        if ((this.tint === 0xff0000 || ball.tint === 0xff0000) && Math.random() < 0.23) {
+            this.tint = 0xff0000
+            ball.tint = 0xff0000
         }
     }
 }
---
+++ src/js/vector.js
@@ -0,0 +1,26 @@
+function add(point1, point2) {
+    const newPoint = point1.clone()
+    newPoint.set(point1.x + point2.x, point1.y + point2.y)
+    return newPoint
+}
+function sub(point1, point2) {
+    const newPoint = point1.clone()
+    newPoint.set(point1.x - point2.x, point1.y - point2.y)
+    return newPoint
+}
+function dot(point1, point2) {
+    return (point1.x * point2.x, point1.y * point2.y)
+}
+function scale(point, k) {
+    const newPoint = point.clone()
+    newPoint.set(k * point.x, k * point.y)
+    return newPoint
+}
+function magnitude(point) {
+    return Math.sqrt(point.x ** 2 + point.y ** 2)
+}
+function normalize(point, k = 1) {
+    return scale(point, 1 / magnitude(point)) 
+}
+
+export {add, sub, dot, magnitude, scale, normalize}
