466fb17eaea9841ae6d0ea32f95e3225d3742de4
J Han <69409369+ilsahaann@users.noreply.github.com>
2021-07-06 22:08:17 +0900

    -

--- src/js/ball.js
+++ src/js/ball.js
@@ -1,21 +1,41 @@
 import * as PIXI from 'pixi.js'
-import { app } from './app'
+import {app} from './app'
+import {add, sub, dot, magnitude, scale, normalize} from './vector.js'
 
-class Ball extends PIXI.Graphics {
+class Ball extends PIXI.Sprite {
     constructor() {
+        const speed=Math.random()*2+2
+        const rotation=Math.random()*Math.PI*2
         super()
         this.r = Math.random() * 10 + 10
         this.x = Math.random() * (app.screen.width - 2 * this.r) + this.r
         this.y = Math.random() * (app.screen.height - 2 * this.r) + this.r
-        this.rotation = Math.random() * Math.PI * 2
-        this.speed = Math.random() * 2 + 2
-        this.vx = this.speed * Math.cos(this.rotation)
-        this.vy = this.speed * Math.sin(this.rotation)
-        this.originalColor = Math.random() * 0x00ffff
+        this.velocity=new PIXI.Point(
+            this.speed * Math.cos(this.rotation),
+            this.speed * Math.sin(this.rotation)
+        )
+        this.originalColor = Math.random()<0.01 ? 0xff0000 : Math.random()*0x00ffff
         this.tint = this.originalColor
         this.beginFill(0xffffff)
-        this.arc(0, 0, this.r, 0, Math.PI * 2)
+        this.arc(0,0,this.r,0,Math.PI*2)
         this.endFill()
+        this.texture=app.renderer.generateTexture(graphic)
+    }
+
+    get vx(){
+        return this.velocity.x
+    }
+
+    get vy(){
+        return this.velocity.y
+    }
+
+    set vx(val){
+        this.velocity.x=val
+    }
+
+    set vy(val){
+        this.velocity.y=val
     }
 
     move() {
@@ -32,21 +52,40 @@ class Ball extends PIXI.Graphics {
         for (const ball of balls.children) {
             const d = Math.hypot(this.x - ball.x, this.y - ball.y)
             if (this !== ball && d < this.r + ball.r) {
-                this.tint = 0xff0000
+                this.bounce(ball)
+                this.contage(ball)
                 break
             }
         }
     }
+
+    bounce(ball){
+        const m=scale(add(this.position, ball.position),0.5)
+        const n1=normalize(sub(this.position, ball.position), this.r)
+        const n2=normalize(sub(this.position, ball.position), this.r)
+
+        this.position=add(m,n1)
+        ball.position=add(m,n2)
+
+        const velocity=this.velocity 
+        this.velocity =ball.velocity
+        ball.velocity =velocity
+    }
+    contage(){
+        if(this.tint==0xff0000||ball.tint==0xff0000&& Math.random()<0.23){
+            this.tint=0xff0000
+            ball.tint=0xff0000
+        }
+    }
 }
+const numBalls=100
+let balls=new PIXI.ParticleContainer(numBalls,{tint:true})
 
-const numBalls = 100
-const balls = new PIXI.Container()
-
-function setupBalls() {
+function setupBalls(){
     app.stage.addChild(balls);
-    for (let i = 0; i < numBalls; i++) {
+    for (let i=0 ; i < numBalls; i++ ) {
         balls.addChild(new Ball())
     }
 }
 
-export { balls, setupBalls }
+export{balls, setupBalls}
